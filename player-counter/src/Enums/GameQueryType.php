65606a3785a77b19ee68fbdddb34f474bc7531ea
Boy132 <mail@boy132.de>
2026-01-13 14:28:21 +0100

    add more game query types

--- player-counter/src/Enums/GameQueryType.php
+++ player-counter/src/Enums/GameQueryType.php
@@ -7,19 +7,34 @@ use Filament\Support\Contracts\HasLabel;
 enum GameQueryType: string implements HasLabel
 {
     case Source = 'source';
+
     case MinecraftJava = 'minecraft';
     case MinecraftBedrock = 'minecraftbe';
+
     case Rust = 'rust';
     case Arma3 = 'arma3';
+    case ArkSe = 'arkse';
+    case Squad = 'squad';
+    case Unturned = 'unturned';
+    case Valheim = 'valheim';
+    case VRising = 'vrising';
+
     case Terraria = 'terraria';
+    case Tshock = 'tshock';
+
     case SAMP = 'samp';
     case MultiTheftAuto = 'mta';
     case FiveMRedM = 'cfx';
+
     case Teamspeak3 = 'teamspeak3';
     case Mumble = 'mumble';
 
     public function getLabel(): string
     {
+        if ($this === self::ArkSe) {
+            return 'ARK: Survival Evolved';
+        }
+
         if ($this === self::SAMP) {
             return 'SA:MP';
         }
